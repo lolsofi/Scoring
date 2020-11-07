5e0323cd8bdb78c8895b167e86327ae79958bb5f
unknown <lolsofi@rambler.ru>
2020-11-07 03:23:19 +0300

    sCards

---
+++ sourse/pug/blocks/sCredit/_sCredit.js
@@ -0,0 +1,13 @@
+let sCreditVue = new Vue({
+		el: '#sCredit',
+		data: {
+			imgSRc: 'img/',
+		},
+		methods: { 
+		},
+		 created: function () { 
+		},
+		computed: {
+		
+		},
+	})
---
+++ sourse/pug/blocks/sDifferences/_sDifferences.js
@@ -0,0 +1,13 @@
+let sDifferencesVue = new Vue({
+		el: '#sDifferences',
+		data: {
+			imgSRc: 'img/',
+		},
+		methods: { 
+		},
+		 created: function () { 
+		},
+		computed: {
+		
+		},
+	})
