bad5c5961f8643e1e8c4efd0e662ca355d51674f
unknown <lolsofi@rambler.ru>
2020-11-09 02:27:19 +0300

    footer

---
+++ sourse/pug/blocks/sOurWork/_sOurWork.js
@@ -0,0 +1,13 @@
+let sOurWorkVue = new Vue({
+		el: '#sOurWork',
+		data: {
+			imgSRc: 'img/',
+		},
+		methods: { 
+		},
+		 created: function () { 
+		},
+		computed: {
+		
+		},
+	})
---
+++ sourse/pug/blocks/sRisks/_sRisks.js
@@ -0,0 +1,13 @@
+let sRisksVue = new Vue({
+		el: '#sRisks',
+		data: {
+			imgSRc: 'img/',
+		},
+		methods: { 
+		},
+		 created: function () { 
+		},
+		computed: {
+		
+		},
+	})
